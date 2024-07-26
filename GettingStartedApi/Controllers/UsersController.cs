2c5446474fc2b0b1691dadfc2b3c2ff74b4bf8c1
DaveTheRpaGuy <dave.morris.atl@gmail.com>
2024-07-26 12:24:11 -0400

    Add project files.

---
+++ GettingStartedApi/Controllers/UsersController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GettingStartedApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
+{
+    // GET: api/Users
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+        List<string> output = new();
+        int loopCount = Random.Shared.Next(2, 10);
+        for (int i = 0; i < loopCount; i++)
+        {
+            output.Add($"Value #{i + 1}");
+        }
+        return output;
+    }
+
+    // GET api/Users/5
+    [HttpGet("{id}")]
+    public string Get(int id)
+    {
+        return $"Value #{id + 1}";
+    }
+
+    // POST api/Users
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+    }
+
+    // PUT api/Users/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
+
+    // PATCH api/Users/5
+    [HttpPatch("{id}")]
+    public void Patch(int id, [FromBody] string emailAddress)
+    {
+
+    }
+
+    // DELETE api/Users/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
+}
